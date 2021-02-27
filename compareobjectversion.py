70fcc83233525c378bac70894e4c797e8bd57d81
Erlangga <erlangga.ip@gmail.com>
2021-02-27 14:28:21 +0700

    [INIT]

---
+++ compareobjectversion.py
@@ -0,0 +1,25 @@
+from odoo_object import OdooObject
+import sys
+
+url1 = sys.argv[1]
+db1 = sys.argv[2]
+username1 = sys.argv[3]
+password1 = sys.argv[4]
+
+url2 = sys.argv[5]
+db2 = sys.argv[6]
+username2 = sys.argv[7]
+password2 = sys.argv[8]
+
+object1 = OdooObject(url1, db1, username1, password1)
+object2 = OdooObject(url2, db2, username2, password2)
+
+partner1_fields = object1.fields_get('res.partner')
+partner2_fields = object2.fields_get('res.partner')
+
+partner1_fields_set = set(partner1_fields.keys())
+partner2_fields_set = set(partner2_fields.keys())
+
+partner_fields_difference_set = partner2_fields_set.difference(partner1_fields_set)
+
+import pdb;pdb.set_trace()
---
+++ odoo_object.py
@@ -0,0 +1,36 @@
+import xmlrpclib
+
+class OdooObject:
+
+	def __init__(self, url, db, username, password):
+		self.url = url
+		self.db = db
+		self.username = username
+		self.password = password
+		self.common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(self.url))
+		print(self.common.version())
+		print('Logging In.....')
+		self.uid = self.common.authenticate(self.db, self.username, self.password, {})
+		self.models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(self.url))
+		# import pdb;pdb.set_trace()
+	
+	def check_access_rights(self, model_name='res.partner'):
+		print('Checking access right of %s:' %model_name),
+		res = self.models.execute_kw(self.db, self.uid, self.password,
+		    model_name, 'check_access_rights',
+		    ['read'], {'raise_exception': False})
+		return res
+
+	def fields_get(self, model_name='res.partner'):
+		print('Fields of model %s:' %model_name)
+		print('=========================================================================')
+		res = self.models.execute_kw(self.db, self.uid, self.password,
+		    model_name, 'fields_get', [])
+		return res
+
+	def call(self, model_name='res.partner', function_name='search', args=[]):
+		res = self.models.execute_kw(self.db, self.uid, self.password,
+		    model_name, function_name, args)
+		return res
+
+
