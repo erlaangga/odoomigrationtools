f6a89a3914e28ba6a26d6dc4e3580093ba876c52
Erlangga <erlangga.ip@gmail.com>
2021-03-01 16:25:11 +0700

    add todo in compare object model

--- README.md
+++ README.md
@@ -8,5 +8,11 @@ Migration can be boring, so I decided to create this to help people migrating fr
 Run compareobjectmodel.py to get comparation of difference model, so we can directly create lists of custom object for future development.
 
 ```
-python compareobjectmodel.py http://localhost:8069 db_default username password http://localhost:8069 db_custom admin 1
+python compareobjectmodel.py source_host source_db source_username source_password target_host target_db target_admin target_password
+```
+
+example:
+
+```
+python compareobjectmodel.py http://localhost:8069 db_default admin 1 http://localhost:8069 db_custom admin 1
 ```
--- compareobjectmodel.py
+++ compareobjectmodel.py
@@ -1,5 +1,6 @@
-from odoo_object import OdooObject
+import csv
 import sys
+from odoo_object import OdooObject
 
 url1 = sys.argv[1]
 db1 = sys.argv[2]
@@ -14,15 +15,27 @@ password2 = sys.argv[8]
 object1 = OdooObject(url1, db1, username1, password1)
 object2 = OdooObject(url2, db2, username2, password2)
 
-partner1_fields = object1.search_read('ir.model', [], ['model'])
-partner2_fields = object2.search_read('ir.model', [], ['model'])
+object1_models = object1.search_read('ir.model', [], ['model'])
+object2_models = object2.search_read('ir.model', [], ['model'])
 
-model1_names = {model['model']:model['id'] for model in partner1_fields}
-model2_names = {model['model']:model['id'] for model in partner2_fields}
+model1_names = {model['model']:model['id'] for model in object1_models}
+model2_names = {model['model']:model['id'] for model in object2_models}
 
 model1_names_set = set(model1_names.keys())
 model2_names_set = set(model2_names.keys())
 
 model2_names_set.difference(model1_names_set)
+
+# Todo
+# save difference model to custom.models.csv
+
+# save every default model to <model>.<name>.csv
+# list of default fields (intersection) in a table
+# give a break after default fields
+# list of custom fields
+
+# csv column format
+# field name, type, relation
+# if field is not many2one, leave it blank
 
 import pdb;pdb.set_trace()
